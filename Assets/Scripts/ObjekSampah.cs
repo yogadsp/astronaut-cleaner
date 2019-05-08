5457e1da8938ad6fbfd9bf5b6b84a0cf1539633b
yogadsp <yogaseptana@gmail.com>
2019-05-08 14:23:41 +0700

    [perbaikan] tembak & spawn
    Perbaikan player hanya bisa menembak 1 kali dengan menekan 1 kali tombol. Perbaikan spawn sampah jika lebih dari 20 detik, sampah akan lebih banyak muncul

--- Assets/Scripts/ObjekSampah.cs
+++ Assets/Scripts/ObjekSampah.cs
@@ -9,6 +9,9 @@ public class ObjekSampah : MonoBehaviour
 
     //Drag in the Bullet Prefab from the Component Inspector.
     public GameObject pisang;
+    int countObject = 0;
+    float timer = 0f;
+    float timerPlay = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +19,25 @@ public class ObjekSampah : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() { 
-    
+    void Update() {
+        countObject++;
+
+        timer += Time.deltaTime;
+        timerPlay += Time.deltaTime;
+
         int rando = Random.Range(0, 2);
 
-        GameObject pisang_temp = Instantiate(pisang, objek[rando].transform.position, objek[rando].transform.rotation) as GameObject;
+        if(timer > 4)
+        {
+            GameObject pisang_temp = Instantiate(pisang, objek[rando].transform.position, objek[rando].transform.rotation) as GameObject;
+            timer = 0;
+        }
+        else if(timer > 2 && timerPlay > 20)
+        {
+            GameObject pisang_temp = Instantiate(pisang, objek[rando].transform.position, objek[rando].transform.rotation) as GameObject;
+            timer = 0;
+        }
+        
 
     }
 }
--- Assets/Scripts/Tembak.cs
+++ Assets/Scripts/Tembak.cs
@@ -22,27 +22,13 @@ public class Tembak : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Tembak"))
+        if (Input.GetKeyDown("space"))
         {
-            //The Bullet instantiation happens here.
+
             GameObject Temporary_Bullet_Handler;
-            //Temporary_Bullet_Handler = Instantiate(laser, laser_emitter.transform.position, Quaternion.Euler(90, 0, 0)) as GameObject;
+
             Temporary_Bullet_Handler = Instantiate(laser, laser_emitter.transform.position, laser_emitter.transform.rotation) as GameObject;
-            //Temporary_Bullet_Handler = Instantiate(laser, laser_emitter, Quaternion.Euler(new Vector3(45, 0, 0)));
 
-            //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
-            //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
-            //Quaternion spawnRotation = Quaternion.Euler(90, 0, 0);
-            //Temporary_Bullet_Handler.transform.rotation = spawnRotation;
-
-            //Retrieve the Rigidbody component from the instantiated Bullet and control it.
-            //Rigidbody Temporary_RigidBody;
-            //Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
-
-            //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
-            //Temporary_RigidBody.AddForce(transform.forward * Laser_Forward_Force);
-
-            //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
             Destroy(Temporary_Bullet_Handler, 0.1f);
         }
     }
